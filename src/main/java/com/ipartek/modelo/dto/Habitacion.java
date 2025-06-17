08faefacc10473975cc2eb6efa60eedcbe547bfd
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2025-06-17 11:35:04 +0200

    creacion view habitacion

--- src/main/java/com/ipartek/modelo/dto/Habitacion.java
+++ src/main/java/com/ipartek/modelo/dto/Habitacion.java
@@ -12,13 +12,10 @@ public class Habitacion {
 	
 	private int id;
 	private String titulo;
-	
 	private int num_habitacion;
 	private int num_camas;
-	
 	private String metros;
 	private double precio;
-	
 	private boolean exterior;
 
 
